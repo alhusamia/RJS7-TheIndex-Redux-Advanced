157356e86119ed9bfe4e510d166ca657181b55cd
PC <fdhfdh>
2020-03-19 15:17:39 +0200

    000

--- src/redux/actions/authors.js
+++ src/redux/actions/authors.js
@@ -1,20 +1,17 @@
 import { SET_AUTHORS } from "./actionTypes";
+
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://the-index-api.herokuapp.com"
+  baseURL: "https://the-index-api.herokuapp.com/"
 });
-export const fetchAuthors = () => {
-   async dispatch => {
-    try {
-      const res = await instance.get("/api/authors/");
-      const authors = res.data;
-      dispatch({
-        type: SET_AUTHORS,
-        payload: authors
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
+
+export const fetchAuthors = () => async dispatch => {
+  try {
+    const res = await instance.get("/api/authors/");
+    const authors = res.data;
+    dispatch({ type: SET_AUTHORS, payload: authors });
+  } catch (error) {
+    console.error(error);
+  }
 };
--- src/redux/actions/books.js
+++ src/redux/actions/books.js
@@ -1,20 +1,17 @@
 import { SET_BOOKS } from "./actionTypes";
+
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://the-index-api.herokuapp.com"
+  baseURL: "https://the-index-api.herokuapp.com/"
 });
-export const fetchBooks = () => {
-  return async dispatch => {
-    try {
-      const res = await instance.get("/api/books/");
-      const books = res.data;
-      dispatch({
-        type: SET_BOOKS,
-        payload: books
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
+
+export const fetchBooks = () => async dispatch => {
+  try {
+    const res = await instance.get("/api/books/");
+    const books = res.data;
+    dispatch({ type: SET_BOOKS, payload: books });
+  } catch (error) {
+    console.error(error);
+  }
 };
